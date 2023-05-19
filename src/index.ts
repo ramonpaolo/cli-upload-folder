4805fe8c6c97ae33086c4440ed2186f5dacf6875
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-05-19 08:41:46 -0300

    fix: change command to flags

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import dotenv from 'dotenv'
 import fs from 'fs'
 import yargs from 'yargs/yargs'
@@ -9,28 +11,14 @@ import file from './file'
 dotenv.config()
 
 const argv = yargs(hideBin(process.argv))
-    .command('bucket [name]', 'name of bucket', (yargs) => {
-        return yargs
-            .positional('bucket', {
-                describe: 'where will be upload the files',
-                default: process.env.BUCKET,
-            })
-    })
-    .command('path-local [path]', 'local path of files or folders', (yargs) => {
-        return yargs
-            .positional('path-local', {
-                describe: 'path to local files',
-                default: './',
-            })
-    })
-    .command('path-upload [path]', 'web path on S3', (yargs) => {
-        return yargs
-            .positional('path-upload', {
-                describe: 'path to store files on S3',
-                default: '/',
-            })
-    })
-    .parse() as any
+    .nargs('path-local', 1)
+    .describe('path-local', 'path to local files')
+    .nargs('path-upload', 1)
+    .describe('path-upload', 'path to web files on S3')
+    .demandOption(['path-local'])
+    .help('h')
+    .alias('h', 'help')
+    .argv as any;
 
 const path = argv['path-local']
 const folderToUpload = argv['path-upload']
